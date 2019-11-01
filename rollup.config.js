5ae43dc8c6c64d50ecb06991227fb0f59f046492
Pipatpol Tanavongchinda <pipatpol@wongnai.com>
2019-11-01 16:01:38 +0700

    Remove support iife

--- rollup.config.js
+++ rollup.config.js
@@ -4,21 +4,28 @@ import includePaths from 'rollup-plugin-includepaths'
 import builtins from 'rollup-plugin-node-builtins'
 import globals from 'rollup-plugin-node-globals'
 
+const globalsLibraries = {
+	parse5: "parse5",
+	lodash: "lodash",
+	http: "http",
+	https: "https",
+	"image-size": "image-size",
+	url: "url",
+	"cache-manager": "cache-manager",
+}
+
 module.exports = {
 	input: 'src/index.ts',
 	output: [
 		{
 			file: 'lib/index.js',
 			format: 'cjs',
+			globals: globalsLibraries
 		},
 		{
 			file: 'lib/index.es.js',
 			format: 'esm',
-		},
-		{
-			file: 'lib/index.iife.js',
-			format: 'iife',
-			name: 'tonakatsu-amp.iife.js',
+			globals: globalsLibraries
 		},
 	],
 	plugins: [
