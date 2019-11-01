9eaa03353b1f2860a2ad56e2ac70ec5a3e32e910
Pipatpol Tanavongchinda <pipatpol@wongnai.com>
2019-11-01 17:50:14 +0700

    Improve walk condition checking

--- src/walk.ts
+++ src/walk.ts
@@ -4,13 +4,13 @@ export async function walk(
 	node: ParentNode,
 	callback: (node: ParentNode | Node) => void | boolean | Promise<void>,
 ): Promise<void | boolean> {
-	let res = await callback(node)
+	const res = await callback(node)
 	if (res === false) {
 		return false
 	} else {
-		let parentNode = node as DefaultTreeParentNode
-		if (parentNode.childNodes) {
-			for (let child of parentNode.childNodes) {
+		const parentNode = node as DefaultTreeParentNode
+		if (!!parentNode.childNodes) {
+			for (const child of parentNode.childNodes) {
 				if ((await walk(child, callback)) === false) {
 					return false
 				}
