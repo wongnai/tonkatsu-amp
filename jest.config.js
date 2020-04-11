8683bf1669937f8c4592d6eeffe49447b4ec821d
PNNutkung <pipatpol.t@ku.th>
2020-04-11 20:37:48 +0700

    Change tslint to eslint

--- jest.config.js
+++ jest.config.js
@@ -1,29 +1,19 @@
 module.exports = {
-	"preset": "ts-jest",
-	"testEnvironment": "node",
-	"transform": {
-		"\\.(ts)$": "ts-jest",
+	preset: 'ts-jest',
+	testEnvironment: 'node',
+	transform: {
+		'\\.(ts)$': 'ts-jest',
 	},
-	"moduleDirectories": [
-		"src",
-		"node_modules"
+	moduleDirectories: ['src', 'node_modules'],
+	testMatch: [
+		'<rootDir>/**/__tests__/**/*.(ts)',
+		'<rootDir>/**/?(*.)(spec|test).(ts)',
 	],
-	"testMatch": [
-		"<rootDir>/**/__tests__/**/*.(ts)",
-		"<rootDir>/**/?(*.)(spec|test).(ts)"
-	],
-	"collectCoverageFrom": [
-		"src/**/*.{ts}"
-	],
-	"transformIgnorePatterns": [
-		"<rootDir>/node_modules/(?!lodash-es/.*)"
-	],
-	"moduleNameMapper": {
-		"^lodash-es$": "<rootDir>/node_modules/lodash/index.js"
+	collectCoverageFrom: ['src/**/*.{ts}'],
+	transformIgnorePatterns: ['<rootDir>/node_modules/(?!lodash-es/.*)'],
+	moduleNameMapper: {
+		'^lodash-es$': '<rootDir>/node_modules/lodash/index.js',
 	},
-	"coverageReporters": [
-		"html",
-		"text"
-	],
-	"verbose": true
+	coverageReporters: ['html', 'text'],
+	verbose: true,
 }
--- rollup.config.js
+++ rollup.config.js
@@ -3,13 +3,13 @@ import typescript from 'rollup-plugin-typescript2'
 import includePaths from 'rollup-plugin-includepaths'
 
 const globalsLibraries = {
-	parse5: "parse5",
-	lodash: "lodash",
-	http: "http",
-	https: "https",
-	"image-size": "image-size",
-	url: "url",
-	"cache-manager": "cache-manager",
+	parse5: 'parse5',
+	lodash: 'lodash',
+	http: 'http',
+	https: 'https',
+	'image-size': 'image-size',
+	url: 'url',
+	'cache-manager': 'cache-manager',
 }
 
 module.exports = {
@@ -18,29 +18,22 @@ module.exports = {
 		{
 			file: 'lib/index.js',
 			format: 'cjs',
-			globals: globalsLibraries
+			globals: globalsLibraries,
 		},
 		{
 			file: 'lib/index.es.js',
 			format: 'esm',
-			globals: globalsLibraries
+			globals: globalsLibraries,
 		},
 	],
 	plugins: [
-		terser({ 
-			exclude: [ '*es*' ]
+		terser({
+			exclude: ['*es*'],
 		}),
 		typescript(),
 		includePaths({
-			paths: [
-				"src"
-			]
+			paths: ['src'],
 		}),
 	],
-	external: [
-		"parse5",
-		"lodash",
-		"image-size",
-		"cache-manager",
-	]
+	external: ['parse5', 'lodash', 'image-size', 'cache-manager'],
 }
